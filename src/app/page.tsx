6f3e58b2956555747d1640a9ea3172ace8c46a11
sineTlsl <limsieun0313@gmail.com>
2023-12-05 15:33:59 +0900

    feat: 팔로잉 바에 carousel 라이브러리 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,11 @@ export default async function HomePage() {
 
   return (
     <section className='flex flex-col md:flex-row max-w-[850px] p-4'>
-      <div className='w-full basis-3/4'>
+      <div className='w-full basis-3/4 min-w-0'>
         <FollowingBar />
         <PostList />
       </div>
-      <div className='basis-1/4'>
+      <div className='basis-1/4 ml-8'>
         <SideBar user={user} />
       </div>
     </section>
--- src/components/FollwingBar.tsx
+++ src/components/FollwingBar.tsx
@@ -5,11 +5,18 @@ import Link from 'next/link';
 import { PacmanLoader } from 'react-spinners';
 import useSWR from 'swr';
 import Avatar from './Avatar';
+import ScrollableBar from './ui/ScrollableBar';
 
 export default function FollowingBar() {
   const { data, isLoading, error } = useSWR<DetailUser>('/api/me');
-   console.log(data?.following);
-  const users = data?.following;
+  console.log(data?.following);
+  // const users = data?.following;
+  // const users = undefined;
+  const users = data?.following && [
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+  ];
 
   // --- 1. 클라이언트 컴포넌트에서 백엔드에게 api/me 사용자의 정보를 얻어옴
   // --- 2. 백엔드에서는 현재 로그인된 사용자의 세션 정보를 이용해서
@@ -18,23 +25,27 @@ export default function FollowingBar() {
   //    (image, username)
 
   return (
-    <section>
+    <section className='w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto'>
       {isLoading ? (
         <PacmanLoader size={8} color='#35917f' />
       ) : (
         (!users || users.length === 0) && <p>{`You don't have following`}</p>
       )}
       {users && users.length > 0 && (
-        <ul>
-          {users.map(({ image, username }, idx) => (
-            <li key={idx}>
-              <Link href={`/user/${username}`}>
-                <Avatar image={image} hightlight />
-                <p>{username}</p>
-              </Link>
-            </li>
+        <ScrollableBar>
+          {users.map(({ image, username }) => (
+            <Link
+              key={username}
+              className='flex flex-col items-center w-20'
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} hightlight />
+              <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>
+                {username}
+              </p>
+            </Link>
           ))}
-        </ul>
+        </ScrollableBar>
       )}
     </section>
   );
