6c2324fa869c69b5dddaa24a0b61bcaca3d453f7
sineTlsl <limsieun0313@gmail.com>
2023-12-27 18:56:27 +0900

    feat: 포스트에 필요한 아이콘 컴포넌트 구현

---
+++ src/components/ui/icons/BookmarkIcon.tsx
@@ -0,0 +1,5 @@
+import { IoBookmarkOutline } from 'react-icons/io5';
+
+export default function BookmarkIcon() {
+  return <IoBookmarkOutline className='w-7 h-7' />;
+}
---
+++ src/components/ui/icons/SmileIcon.tsx
@@ -0,0 +1,5 @@
+import { FaRegFaceSmile } from 'react-icons/fa6';
+
+export default function SmileIcon() {
+  return <FaRegFaceSmile className='w-7 h-7' color='#000' />;
+}
