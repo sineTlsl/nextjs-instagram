52d6d90ed2d10210302d5120d844818f43e97698
sineTlsl <limsieun0313@gmail.com>
2023-12-27 21:25:31 +0900

    style: 앱 전체적인 스타일 수정

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -22,8 +22,8 @@ export default async function SignInPage({
   const providers = (await getProviders()) ?? {};
 
   return (
-    <section className='w-full'>
-      <div className='w-full mt-[30%] mx-auto flex flex-col items-center justify-center gap-2 w-full-mobile sm:max-w-sm px-2'>
+    <section className='w-full'>  
+      <div className='w-full mt-[20%] mx-auto flex flex-col items-center justify-center gap-2 w-full-mobile sm:max-w-sm px-2'>
         <SignIn providers={providers} callbackUrl={callbackUrl ?? '/'} />
       </div>
     </section>
