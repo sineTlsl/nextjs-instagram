f10b5a6fae8f432cb57d8f2c5605c6ad719350e4
sineTlsl <limsieun0313@gmail.com>
2023-12-27 19:21:14 +0900

    refactor: 포스트 목록 리팩토링

---
+++ src/components/CommentForm.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import SmileIcon from './ui/icons/SmileIcon';
+
+export default function CommentForm() {
+  return (
+    <form className='flex border-t px-3 items-center text-neutral-300'>
+      <SmileIcon />
+      <input
+        className='w-full ml-2 border-none outline-none p-3'
+        type='text'
+        placeholder='Add a comment...'
+      />
+      <button className='font-bold text-sky-500 ml-2'>Post</button>
+    </form>
+  );
+}
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { SimplePost } from '@/model/posts';
-import { GridLoader } from 'react-spinners';
 import useSWR from 'swr';
 import PostListCard from './PostListCard';
+import GridSpinner from './GridSpinner';
 
 export default function PostList() {
   const { data: posts, isLoading } = useSWR<SimplePost[]>('/api/posts');
@@ -12,14 +12,14 @@ export default function PostList() {
     <section>
       {isLoading && (
         <div className='text-center mt-32'>
-          <GridLoader color='red' />
+          <GridSpinner />
         </div>
       )}
       <ul>
         {posts &&
-          posts.map((post) => (
+          posts.map((post, idx) => (
             <li key={post.id} className='mb-5'>
-              <PostListCard post={post} />
+              <PostListCard post={post} priority={idx < 2} />
             </li>
           ))}
       </ul>
