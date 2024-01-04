ef3cb8ed36ceaa7b55cdd01673931ecfdb6c25b9
sineTlsl <limsieun0313@gmail.com>
2024-01-04 19:44:44 +0900

    refactor: usePosts hook optimisticData 사용

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,19 +1,36 @@
-import useSWR from "swr";
 import { SimplePost } from "@/model/posts";
-import { useSWRConfig } from "swr";
+import useSWR, { useSWRConfig } from "swr";
+
+async function updateLike(id: string, like: boolean) {
+  return fetch("/api/likes", {
+    method: "PUT",
+    body: JSON.stringify({ id, like }),
+  }).then(res => res.json());
+}
 
 export default function usePosts() {
-  const { data: posts, isLoading, error } = useSWR<SimplePost[]>("/api/posts");
-  const { mutate } = useSWRConfig();
+  const {
+    data: posts,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR<SimplePost[]>("/api/posts");
 
   const setLike = (post: SimplePost, username: string, like: boolean) => {
-    fetch(`/api/likes`, {
-      method: "PUT",
-      body: JSON.stringify({
-        id: post.id,
-        like,
-      }),
-    }).then(() => mutate("/api/posts"));
+    const newPost = {
+      ...post,
+      likes: like
+        ? [...post.likes, username]
+        : post.likes.filter(item => item !== username),
+    };
+    const newPosts = posts?.map(p => (p.id === post.id ? newPost : p));
+
+    return mutate(updateLike(post.id, like), {
+      optimisticData: newPosts, // 즉각적으로 UI를 업데이트 되도록
+      populateCache: false,
+      revalidate: false,
+      rollbackOnError: true, // 네트워크상 문제가 생겨 백엔드에 업데이트가 안되었다면 에러 true
+    });
   };
 
   return { posts, isLoading, error, setLike };
