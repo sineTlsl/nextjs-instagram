7c25c90d6203ae8795d40b18b876d13f936dc496
임시은 <limsieun@imsieun-ui-noteubug.local>
2024-01-16 18:52:34 +0900

    style: prettier 변경

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,17 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
-import { addUser } from "@/service/user";
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
+import { addUser } from '@/service/user';
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
     GithubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
     }),
   ],
   callbacks: {
@@ -23,34 +23,27 @@ export const authOptions: NextAuthOptions = {
       addUser({
         id,
         email,
-        name: name || "",
-        username: email.split("@")[0],
+        name: name || '',
+        username: email.split('@')[0],
         image,
       });
 
       return true;
     },
-    async session({ session, token }) {
+    async session({ session }) {
       const user = session?.user;
       if (user) {
         session.user = {
           ...user,
-          username: user.email?.split("@")[0] || "",
-          id: token.id as string,
+          username: user.email?.split('@')[0] || '',
         };
       }
 
       return session;
     },
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
   },
   pages: {
-    signIn: "/auth/signin",
+    signIn: '/auth/signin',
   },
 };
 
--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,20 +1,20 @@
-import { getFollowingPostsOf, getPost, likePost } from "@/service/posts";
-import { getServerSession } from "next-auth";
-import { NextRequest, NextResponse } from "next/server";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import { getFollowingPostsOf, getPost } from '@/service/posts';
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
+import { authOptions } from '../../auth/[...nextauth]/route';
 
 type Context = {
   params: { id: string };
 };
 
-export async function GET(_: NextRequest, context: Context) {
+export async function GET(request: NextRequest, context: Context) {
   const session = await getServerSession(authOptions);
   const user = session?.user;
 
   if (!user) {
-    return new Response("Authentication Error", { status: 401 });
+    return new Response('Authentication Error', { status: 401 });
   }
 
   return getPost(context.params.id) //
-    .then(data => NextResponse.json(data));
+    .then((data) => NextResponse.json(data));
 }
