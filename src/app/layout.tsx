0389e57ebf65c58e64124de5d54e964fc579a547
sieun <limsieun0313@gmail.com>
2024-02-18 23:33:26 +0900

    remove: 폴더 app/src/context -> app/context로 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Metadata } from 'next';
 
 // components
 import NavBar from '@/components/NavBar';
-import AuthContext from './context/AuthContext';
-import SWRConfigContext from './context/SWRConfigContext';
+import AuthContext from '@/context/AuthContext';
+import SWRConfigContext from '@/context/SWRConfigContext';
 
 const openSans = Open_Sans({ subsets: ['latin'] });
 
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
     default: 'Sinegram',
     template: 'Sinegram | %s',
   },
-  description: 'Sinegram Photos'
+  description: 'Sinegram Photos',
 };
 
 export default function RootLayout({
@@ -23,19 +23,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='ko' className={openSans.className}>
-      <body className='w-full bg-neutral-50 overflow-auto'>
+    <html lang="ko" className={openSans.className}>
+      <body className="w-full bg-neutral-50 overflow-auto">
         <AuthContext>
-          <header className='sticky top-0 bg-white z-10 border-b'>
-            <div className='max-w-screen-xl mx-auto'>
+          <header className="sticky top-0 bg-white z-10 border-b">
+            <div className="max-w-screen-xl mx-auto">
               <NavBar />
             </div>
           </header>
-          <main className='w-full flex max-w-screen-xl justify-center mx-auto'>
+          <main className="w-full flex max-w-screen-xl justify-center mx-auto">
             <SWRConfigContext>{children}</SWRConfigContext>
           </main>
         </AuthContext>
-        <div id='portal' />
+        <div id="portal" />
       </body>
     </html>
   );
