a8d068a6d91ce3492980ae68f26ae6a48e111073
sieun <limsieun0313@gmail.com>
2024-02-20 15:09:11 +0900

    refactor: session 중복되는 로직 유틸함수로 분리

--- src/app/api/likes/route.ts
+++ src/app/api/likes/route.ts
@@ -1,25 +1,19 @@
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { NextRequest, NextResponse } from 'next/server';
 import { disLikePost, likePost } from '@/service/posts';
+import { withSessionUser } from '@/util/session';
 
 export async function PUT(req: NextRequest) {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
+  return withSessionUser(async user => {
+    const { id, like } = await req.json();
 
-  if (!user) {
-    return new Response('Authentication Error', { status: 401 });
-  }
+    if (!id || like === undefined) {
+      return new Response('Bad request', { status: 400 });
+    }
 
-  const { id, like } = await req.json();
+    const request = like ? likePost : disLikePost;
 
-  if (!id || like === undefined) {
-    return new Response('Bad request', { status: 400 });
-  }
-
-  const request = like ? likePost : disLikePost;
-
-  return request(id, user.id) //
-    .then(res => NextResponse.json(res))
-    .catch(err => new Response(JSON.stringify(err), { status: 500 }));
+    return request(id, user.id) //
+      .then(res => NextResponse.json(res))
+      .catch(err => new Response(JSON.stringify(err), { status: 500 }));
+  });
 }
---
+++ src/util/session.ts
@@ -0,0 +1,16 @@
+import { AuthUser } from '@/model/user';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+
+export async function withSessionUser(
+  handler: (user: AuthUser) => Promise<Response>,
+) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response('Authentication Error', { status: 401 });
+  }
+
+  return handler(user);
+}
